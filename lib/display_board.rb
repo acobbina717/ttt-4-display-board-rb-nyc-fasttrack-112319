bcdc6ae7c3d0f17ed2a2b92097a2362424f78863
Abeku Cobbina <acobbina717@gmail.com>
2019-11-26 07:14:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
-board = [" "," "," "," "," "," "," "," "," "]
+board = ["O","X"," "," "," "," "," "," "," "]
 
 def display_board(board_items)
-puts " #{board_items[0]} | #{board_items[1]}  | #{board_items[2]}  "
+puts " #{board_items[0]} | #{board_items[1]} | #{board_items[2]}  "
 puts "-----------"
-puts " #{board_items[3]} | #{board_items[4]}  | #{board_items[5]}  "
+puts " #{board_items[3]} | #{board_items[4]} | #{board_items[5]}  "
 puts "-----------"
-puts " #{board_items[6]} | #{board_items[7]}  | #{board_items[8]}  "
+puts " #{board_items[6]} | #{board_items[7]} | #{board_items[8]}  "
 end
 
 display_board(board)
